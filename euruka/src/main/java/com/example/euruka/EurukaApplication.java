b948b8af71e4d74198e5cf3bc4f727682aac9df8
unknown <tuf3070@example.com>
2024-07-19 22:07:20 +0800

    update

---
+++ euruka/src/main/java/com/example/euruka/EurukaApplication.java
@@ -0,0 +1,14 @@
+package com.example.euruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @EnableEurekaServer
+public class EurukaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurukaApplication.class, args);
+	}
+
+}
